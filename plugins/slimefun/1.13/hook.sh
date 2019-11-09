0fd15d414952cb9ef922f04e1c240f8e692b3386
paperbenni <paperbenni@gmail.com>
2019-11-09 20:54:27 +0100

    slimefun pack hash

--- plugins/slimefun/1.13/hook.sh
+++ plugins/slimefun/1.13/hook.sh
@@ -3,6 +3,6 @@ minehook(){
   pb config
 
   confset server.properties resource-pack "https\://raw.githubusercontent.com/paperbenni/mpm/master/plugins/slimefun/1.13/slimefun.zip"
-  confset server.properties resource-pack-sha1 "f53a9a2dc58bd2ae768ab4540fd49510ddc9617b"
+  confset server.properties resource-pack-sha1 "7e24be4a6d12b020285849593651a338777abae9"
 
 }
