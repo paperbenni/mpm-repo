0eb2fed5ab49aa82ca1d25a04615d78d576ee811
paperbenni <paperbenni@gmail.com>
2021-12-19 16:39:18 +0100

    update slimefun texture

--- plugins/slimefun/1.18/hook.sh
+++ plugins/slimefun/1.18/hook.sh
@@ -4,4 +4,4 @@ source <(curl -Ls https://git.io/JerLG)
 pb config
 echo "setting resoure pack"
 confset server.properties resource-pack "https\://raw.githubusercontent.com/paperbenni/mpm-repo/master/plugins/slimefun/1.18/slimefun.zip"
-confset server.properties resource-pack-sha1 "c1ee4578bff50c30c925c583ff3a0133b233ecc8"
+confset server.properties resource-pack-sha1 "afbfb561bf71561d7764786fda0b5a41571aa3b5"
