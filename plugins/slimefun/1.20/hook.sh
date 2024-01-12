ae331175fd9f0f6fafe4fa43d35f2900b47702e7
paperbenni <paperbenni@gmail.com>
2024-01-12 20:25:09 +0100

    update all the stuff

--- plugins/slimefun/1.20/hook.sh
+++ plugins/slimefun/1.20/hook.sh
@@ -3,6 +3,6 @@ cd ..
 source <(curl -Ls https://git.io/JerLG)
 pb config
 echo "setting resoure pack"
-confset server.properties resource-pack "https\://raw.githubusercontent.com/paperbenni/mpm-repo/master/plugins/slimefun/1.19/slimefun.zip"
-confset server.properties resource-pack-sha1 "afbfb561bf71561d7764786fda0b5a41571aa3b5"
+confset server.properties resource-pack "https\://raw.githubusercontent.com/paperbenni/mpm-repo/master/plugins/slimefun/1.20/slimefun.zip"
+confset server.properties resource-pack-sha1 "865ed80c887547b7fe284ba190ba87d37be3b116"
 
